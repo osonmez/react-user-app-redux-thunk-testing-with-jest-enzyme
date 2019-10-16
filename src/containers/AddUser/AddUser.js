ebba72022856d754a70e53327a9055bb2f59a817
Sonmez <menelaus_1988@hotmail.com>
2019-10-16 11:06:47 +0300

    added style to AddUser

--- src/containers/AddUser/AddUser.js
+++ src/containers/AddUser/AddUser.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import UserForm from '../../components/UserForm/UserForm';
 
+import classes from './AddUser.module.css';
+
 class AddUser extends Component {
 
     render() {
         return (
-            <div>
+            <div className={classes.AddUser}>
                 <UserForm submit={(e) => {e.preventDefault()}}/>
             </div>
         )
