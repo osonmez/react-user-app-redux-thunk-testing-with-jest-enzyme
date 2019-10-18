364c65e172c97371c4b5430546204eedddd44726
Sonmez <menelaus_1988@hotmail.com>
2019-10-18 11:39:10 +0300

    fetch user reducer methods completed.

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -2,16 +2,34 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     users: [],
-    loading:false,
+    loading: false,
     edit: false,
-    selectedUser: null
+    selectedUser: null,
+    error: null
+};
+
+
+const fetchUsersStart = (state) => {
+    return { ...state, loading: true };
+};
+
+const fetchUsersSuccess = (state, action) => {
+    return { ...state, users: action.users, loading: false };
+};
+
+const fetchUsersFail = (state, action) => {
+    return { ...state, loading: false, error: action.error };
 };
 
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case actionTypes.FETCH_USERS:
-            return state;
+        case actionTypes.FETCH_USERS_START:
+            return fetchUsersStart(state);
+        case actionTypes.FETCH_USERS_SUCCESS:
+            return fetchUsersSuccess(state, action);
+        case actionTypes.FETCH_USERS_FAIL:
+            return fetchUsersFail(state, action);
         default:
             return state;
     }
--- src/store/reducers/user.test.js
+++ src/store/reducers/user.test.js
@@ -1,15 +1,52 @@
 import * as actionTypes from '../actions/actionTypes';
 import userReducer from './user';
+import { testUserArray } from '../../shared/testData';
+
+const testState = {
+    users: [],
+    loading: false,
+    edit: false,
+    selectedUser: null,
+    error: null
+};
 
 describe('User Reducer', () => {
 
     it('Should return default state', () => {
         const iState = userReducer(undefined, {});
-        expect(iState).toStrictEqual({
-            users: [],
-            loading:false,
-            edit: false,
-            selectedUser: null
+        expect(iState).toStrictEqual(testState);
+    });
+
+    it('Should fetch users start', () => {
+        const newState = userReducer(undefined, {
+            type: actionTypes.FETCH_USERS_START
+        });
+        expect(newState).toStrictEqual({
+            ...testState,
+            loading: true
+        });
+    });
+
+    it('Should fetch users success', () => {
+        const newState = userReducer(undefined, {
+            type: actionTypes.FETCH_USERS_SUCCESS,
+            users: testUserArray
+        });
+        expect(newState).toStrictEqual({
+            ...testState,
+            users: testUserArray,
+            loading: false
+        });
+    });
+
+    it('Should fetch users failed', () => {
+        const newState = userReducer(undefined, {
+            type: actionTypes.FETCH_USERS_FAIL,
+            error: 'TEST ERROR!'
+        });
+        expect(newState).toStrictEqual({
+            ...testState,
+            error: 'TEST ERROR!'
         });
     });
 
